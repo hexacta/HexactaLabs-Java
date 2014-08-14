d398d8620b18c8ce53cb562c7ef2c7f777944708
Tatiana <tatii.oxo@gmail.com>
2014-08-14 11:24:55 -0300

    Datepicker

--- dev/Tpl/src/main/webapp/app/controller/lendBookCtrl.js
+++ dev/Tpl/src/main/webapp/app/controller/lendBookCtrl.js
@@ -6,3 +6,36 @@ booksApp.controller('lendBookCtrl', function ($scope,$location,$rootScope) {
     };
     
 });
+
+var DatepickerDemoCtrl = function ($scope) {
+	$scope.today = function() {
+    $scope.fromDt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.fromDt = null;
+    $scope.untilDt = null;
+  };
+
+  $scope.openFrom = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.openedFrom = true;
+  };
+
+  $scope.openUntil = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.openedUntil = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.initDate = new Date('2016-15-20');
+  $scope.formats = ['dd MMMM yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+};
