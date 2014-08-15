92fab32b945b637dbaadd2770fa714bd9e0183eb
mbuchwald <mbuchwald@hexacta.com>
2014-08-15 10:12:30 -0300

    Carga dinamica de imagenes usando el servicio rest de google books.

--- dev/Tpl/src/main/webapp/app/controller/bookListCtrl.js
+++ dev/Tpl/src/main/webapp/app/controller/bookListCtrl.js
@@ -26,6 +26,23 @@ booksApp.controller('bookListCtrl', function ($scope,$location,$rootScope,$http)
 
 	$scope.modifyModal=function(book){
 		$scope.selectedBook = book;
+		
+		var find = '-';
+		var re = new RegExp(find, 'g');
+		var isbn = $scope.selectedBook.isbn.replace(re , '');
+		$http({
+			method : 'GET',
+			url: 'https://www.googleapis.com/books/v1/volumes?q=isbn+' + isbn,
+			headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
+			}).success(function(data, status, headers, config){
+				if (status == 200 ){
+					$scope.selectedBook.image = data.items[0].volumeInfo.imageLinks.thumbnail;
+				}
+			}).error(function(data, status, headers, config){
+	    		console.log("An Error occurred while trying to connect to Google API");
+	    		$scope.selectedBook.image = "";
+	    	}) ;
+		
 	};
 	
 	$scope.loadBooks = function(){
@@ -35,7 +52,7 @@ booksApp.controller('bookListCtrl', function ($scope,$location,$rootScope,$http)
 			headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
 		}).success(function(data, status, headers, config){
 
-			if(status = 200)
+			if(status == 200)
 			{
 				$rootScope.books = [];
 				$rootScope.books = data;
@@ -58,7 +75,7 @@ booksApp.controller('bookListCtrl', function ($scope,$location,$rootScope,$http)
 		
 		var jsonComment = angular.toJson($scope.comment);
 		$http.post('/Tpl/rest/comments', jsonComment).success(function(data, status, headers, config){
-    		if(status = 200){
+    		if(status == 200){
     			console.log("Comment Creation Completed.");
     		}
     	}).error(function(data, status, headers, config){
