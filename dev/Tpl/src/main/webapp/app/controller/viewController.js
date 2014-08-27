e41c5e93b1064e557327766676660d8606a7605b
Federico Doll <fdoll@hexacta.com>
2014-08-27 13:33:20 -0300

    merge arreglado

--- dev/Tpl/src/main/webapp/app/controller/viewController.js
+++ dev/Tpl/src/main/webapp/app/controller/viewController.js
@@ -5,7 +5,7 @@ booksApp.controller('viewController', function($scope, $http, $rootScope, $sessi
 	if( $sessionStorage.user !== undefined){
 		$rootScope.usuario = "Bienvenido: " + JSON.parse($sessionStorage.user).username + "!"; 
 	}
-/*	$scope.mostrarLogin = function(){
+	$scope.mostrarLogin = function(){
 		return !sessionStorage.loggedIn;
 	};
 
@@ -17,7 +17,7 @@ booksApp.controller('viewController', function($scope, $http, $rootScope, $sessi
 		if(sessionStorage.loggedIn){
 			return $scope.user = JSON.parse(sessionStorage.user).username; 
 		}	
-	};*/
+	};
 
 	$scope.desloguearse = function(){
 		$rootScope.loggedIn = false;
@@ -34,27 +34,19 @@ booksApp.controller('viewController', function($scope, $http, $rootScope, $sessi
 			headers : {'Authorization' : 'Basic ' + encoded, 'Accept' : 'application/json'}
 		}).success(function(data, status, headers, config){
 			if(status == 200){
-<<<<<<< HEAD
 				//TODO:
 				$sessionStorage.user = JSON.stringify(data);
 				//$sessionStorage.loggedIn = true;
 				$scope.usuario = "Bienvenido: " + JSON.parse($sessionStorage.user).username + "!"; 
 				$rootScope.loggedIn = true;
+				$scope.invalidLogin = false;
 				
 			}
 		}).error(function(data, status, headers, config){
-			alert("El usuario " + user.username + " no esta registrado o escribio mal la password.");
-			$scope.user.password="";
-=======
-				sessionStorage.user = JSON.stringify(data);
-				sessionStorage.loggedIn = true;
-				$scope.invalidLogin = false;
-			}
-		}).error(function(data, status, headers, config){
 			console.log("An Error occurred while trying to login");
+			$scope.user.password="";
 			$scope.invalidLogin = true;
-			user.password = "";
->>>>>>> d8c65780a38deb4ffac8ae9e98c028eb0e4195a9
+
 		});
 	};
 
