26a8f986949ebfbbbad251a1aa4286abb19d4de4
Federico Doll <fdoll@hexacta.com>
2014-09-03 14:04:25 -0300

    2/9

--- dev/Tpl/src/main/java/ar/com/hexacta/tpl/model/builder/BookBuilder.java
+++ dev/Tpl/src/main/java/ar/com/hexacta/tpl/model/builder/BookBuilder.java
@@ -6,75 +6,73 @@ import java.util.Set;
 import ar.com.hexacta.tpl.model.Book;
 import ar.com.hexacta.tpl.model.BookCategory;
 import ar.com.hexacta.tpl.model.BookCopy;
-import ar.com.hexacta.tpl.model.Comment;
 
 public class BookBuilder {
 
-    private String name = "defaultName";
+	private String name = "defaultName";
 
-    private String description = "defaultDescription";
+	private String description = "defaultDescription";
 
-    private String publisher = "defaultPublisher";
+	private String publisher = "defaultPublisher";
 
-    private String country = "defaultCountry";
-    
-    private String isbn = "defaultISBN";
+	private String country = "defaultCountry";
 
-    private Set<BookCategory> bookCategories = new HashSet<BookCategory>(0);
+	private String isbn = "defaultISBN";
 
-    private Set<BookCopy> bookCopies = new HashSet<BookCopy>(0);
+	private Set<BookCategory> bookCategories = new HashSet<BookCategory>(0);
 
-    private Set<Comment> bookComments = new HashSet<Comment>(0);
+	private Set<BookCopy> bookCopies = new HashSet<BookCopy>(0);
 
-    public Book build() {
-        return new Book(name, description, publisher, country, isbn, bookCategories, bookCopies, bookComments);
-    }
+	public Book build() {
+		return new Book(name, description, publisher, country, isbn,
+				bookCategories, bookCopies);
+	}
 
-    public BookBuilder withBookCopy(final BookCopy... aCopy) {
-        for (BookCopy bookCopy : aCopy) {
-            if (bookCopy.getState().equals(BookCopy.STATE_FREE)) {
-                bookCopies.add(bookCopy);
-            }
-        }
-        return this;
-    }
+	public BookBuilder withBookCopy(final BookCopy... aCopy) {
+		for (BookCopy bookCopy : aCopy) {
+			if (bookCopy.getState().equals(BookCopy.STATE_FREE)) {
+				bookCopies.add(bookCopy);
+			}
+		}
+		return this;
+	}
 
-    public Set<BookCopy> getFreeBookCopies() {
-        for (BookCopy bookCopy : bookCopies) {
-            if (bookCopy.getState().equals(BookCopy.STATE_FREE)) {
-                bookCopies.add(bookCopy);
-            }
-        }
-        return bookCopies;
-    }
+	public Set<BookCopy> getFreeBookCopies() {
+		for (BookCopy bookCopy : bookCopies) {
+			if (bookCopy.getState().equals(BookCopy.STATE_FREE)) {
+				bookCopies.add(bookCopy);
+			}
+		}
+		return bookCopies;
+	}
 
-    public BookBuilder withCategory(final BookCategory aCategory) {
-        bookCategories.add(aCategory);
-        return this;
-    }
+	public BookBuilder withCategory(final BookCategory aCategory) {
+		bookCategories.add(aCategory);
+		return this;
+	}
 
-    public BookBuilder withDescription(final String aDescription) {
-        description = aDescription;
-        return this;
-    }
+	public BookBuilder withDescription(final String aDescription) {
+		description = aDescription;
+		return this;
+	}
 
-    public BookBuilder withName(final String aName) {
-        name = aName;
-        return this;
-    }
+	public BookBuilder withName(final String aName) {
+		name = aName;
+		return this;
+	}
 
-    public BookBuilder withPublisher(final String aPublisher) {
-        publisher = aPublisher;
-        return this;
-    }
-    
-    public BookBuilder withCountry(final String aCountry){
-    	country = aCountry;
-    	return this;
-    }
-    
-    public BookBuilder withISBN(final String aISBN){
-    	isbn = aISBN;
-    	return this;
-    }
+	public BookBuilder withPublisher(final String aPublisher) {
+		publisher = aPublisher;
+		return this;
+	}
+
+	public BookBuilder withCountry(final String aCountry) {
+		country = aCountry;
+		return this;
+	}
+
+	public BookBuilder withISBN(final String aISBN) {
+		isbn = aISBN;
+		return this;
+	}
 }
--- dev/Tpl/src/main/java/ar/com/hexacta/tpl/model/builder/BookCategoryBuilder.java
+++ dev/Tpl/src/main/java/ar/com/hexacta/tpl/model/builder/BookCategoryBuilder.java
@@ -4,22 +4,22 @@ import ar.com.hexacta.tpl.model.BookCategory;
 
 public class BookCategoryBuilder {
 
-    private String name = "defaultName";
+	private String name = "defaultName";
 
-    private String description = "defaultDescription";
+	private String description = "defaultDescription";
 
-    public BookCategoryBuilder withName(final String aName) {
-        name = aName;
-        return this;
-    }
+	public BookCategoryBuilder withName(final String aName) {
+		name = aName;
+		return this;
+	}
 
-    public BookCategoryBuilder withDescription(final String aDescription) {
+	public BookCategoryBuilder withDescription(final String aDescription) {
 
-        description = aDescription;
-        return this;
-    }
+		description = aDescription;
+		return this;
+	}
 
-    public BookCategory buid() {
-        return new BookCategory(name, description);
-    }
+	public BookCategory build() {
+		return new BookCategory(name, description);
+	}
 }
