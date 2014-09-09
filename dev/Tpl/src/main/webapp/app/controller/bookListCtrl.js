93102598d75a51e76573369a7720bbc4fff5296b
Tatiana <tatii.oxo@gmail.com>
2014-09-09 12:52:42 -0300

    conseguir copias disponibles

--- dev/Tpl/src/main/webapp/app/controller/bookListCtrl.js
+++ dev/Tpl/src/main/webapp/app/controller/bookListCtrl.js
@@ -24,56 +24,107 @@ booksApp.controller('bookListCtrl', function ($scope,$location,$rootScope,$http)
 		headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
 	}).success(function(data, status, headers, config){
 		
+		//Cargo los libros en el scope
+		$scope.loadBooks = function(){
+			$rootScope.books = [];
+			$rootScope.books = data;
+			$scope.books = $rootScope.books;
+			$scope.predicate = 'name';
+			$scope.reverse = false;
+			
+			$scope.comment = {};
+		}
+		$scope.loadBooks();
+		
+		//Getting availables copies
+		$scope.availableBook = [];
+		$scope.copy = [];
+		
+		$scope.getCopies = function(){
+			angular.forEach($scope.books, function(value, key){
+				$http({
+					method: 'GET',
+					url : '/Tpl/rest/copies/book/' + value.id,
+					headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
+				}).success(function(data, status, headers, config){
+					if (status == 200 || status == 204){
+						$scope.copy = data;
+						if(angular.equals($scope.copy.state, "free")){
+							$scope.availableBook.push($scope.copy);
+						}
+					}
+				}).error(function(data, status, headers, config){
+					console.log("An error ocurred while trying to get copies");
+				});
+				console.log($scope.availableBook);
+			});	
+		}
+
+		$scope.getCopies();
+		
+		
+		/*
+		$scope.copies = [];
+		$scope.getCopies = function(){
+			$http({
+				method: 'GET',
+				url : '/Tpl/rest/copies',
+				headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
+			}).success(function(data, status, headers, config){
+				if (status == 200 || status == 204){
+					$scope.copies = data;
+					$scope.getAvailables();
+				}
+			}).error(function(data, status, headers, config){
+				console.log("An error ocurred while trying to get copies");
+			});
+		}
+		$scope.getCopies();
+		$scope.getAvailables = function(){
+			angular.forEach($scope.copies, function(value, key){
+				if( angular.equals(value.state, "Free")){
+				
+			});
+		}
+	*/
+		
 		//Modal
-	$scope.modifyModal=function(book){
-		$scope.selectedBook = book;
-		$scope.selectedBook.image = "";
+		$scope.modifyModal=function(book){
+			$scope.selectedBook = book;
+			$scope.selectedBook.image = "";
 		
-		//Getting comments
-		var bookId = $scope.selectedBook.id;
-		$http({
-			method : 'GET',
-			url: '/Tpl/rest/comments/byBook/' + bookId,
-			headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
-			}).success(function(data, status, headers, config){
-				if (status == 200 ){
-					$scope.selectedBook.bookComments = data;
-				}
-			}).error(function(data, status, headers, config){
-	    		console.log("An Error occurred while trying to get comments for the book " + $scope.selectedBook.name);
-	    		$scope.selectedBook.comments = "";
-	    	});
-		
-		// Get image from google rest service
-		var find = '-';
-		var re = new RegExp(find, 'g');
-		var isbn = $scope.selectedBook.isbn.replace(re , '');
-		$http({
-			method : 'GET',
-			url: 'https://www.googleapis.com/books/v1/volumes?q=isbn+' + isbn,
-			headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
-			}).success(function(data, status, headers, config){
-				if (status == 200 ){
-					$scope.selectedBook.image = data.items[0].volumeInfo.imageLinks.thumbnail;
-				}
-			}).error(function(data, status, headers, config){
-	    		console.log("An Error occurred while trying to connect to Google API");
-	    		$scope.selectedBook.image = "";
-	    	}) ;
-	};
-	
-	//Cargo los libros en el scope
-	$scope.loadBooks = function(){
-		$rootScope.books = [];
-		$rootScope.books = data;
-		$scope.books = $rootScope.books;
-	}
-	$scope.loadBooks();
-	
-	$scope.predicate = 'name';
-	$scope.reverse = false;
-	
-	$scope.comment = {};
+			//Getting comments
+			var bookId = $scope.selectedBook.id;
+			$http({
+				method : 'GET',
+				url: '/Tpl/rest/comments/byBook/' + bookId,
+				headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
+				}).success(function(data, status, headers, config){
+					if (status == 200 ){
+						$scope.selectedBook.bookComments = data;
+					}
+				}).error(function(data, status, headers, config){
+		    		console.log("An Error occurred while trying to get comments for the book " + $scope.selectedBook.name);
+		    		$scope.selectedBook.comments = "";
+		    	});
+			
+			// Get image from google rest service
+			var find = '-';
+			var re = new RegExp(find, 'g');
+			var isbn = $scope.selectedBook.isbn.replace(re , '');
+			$http({
+				method : 'GET',
+				url: 'https://www.googleapis.com/books/v1/volumes?q=isbn+' + isbn,
+				headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
+				}).success(function(data, status, headers, config){
+					if (status == 200 ){
+						$scope.selectedBook.image = data.items[0].volumeInfo.imageLinks.thumbnail;
+					}
+				}).error(function(data, status, headers, config){
+		    		console.log("An Error occurred while trying to connect to Google API");
+		    		$scope.selectedBook.image = "";
+		    	});
+		};
 	
 	//Para agregar comentarios
 	$scope.addComment = function(book){
@@ -96,9 +147,12 @@ booksApp.controller('bookListCtrl', function ($scope,$location,$rootScope,$http)
 		book.bookComments = comments;
 		$scope.selectedBook.image = image;
 		$scope.loadBooks();
-
-	$scope.actionsAllowed = function(){
-		return sessionStorage.loggedIn;
 	};
 	
+	$scope.actionsAllowed = function(){
+		return sessionStorage.loggedIn;
+	};	
+	}).error(function(data, status, headers, config){
+		console.log("An Error occurred while trying to get the books");
+	});
 });
