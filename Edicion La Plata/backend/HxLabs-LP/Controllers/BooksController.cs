e9068495b7913501a19b3542e987d9d674764e6d
tatiana <tinama@hexacta.com>
2014-10-17 13:20:13 -0300

    back y front de HX-Labs de La Plata

---
+++ Edicion La Plata/backend/HxLabs-LP/Controllers/BooksController.cs
@@ -0,0 +1,66 @@
+﻿using HxLabs_LP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace HxLabs_LP.Controllers
+{
+    public class BooksController : ApiController
+    {
+        public static List<Book> books = new List<Book>();
+        public static int PageLoad = 1;
+
+        public BooksController()
+        {
+            
+            books.Add( new Book { 
+                id = 1,
+                title = "El Principito", 
+                description = "Best-seller del escritor frances Antoine de Saint-Exupery. ",
+                genre = "Fabula", 
+                publisher = "Editorial Planeta"
+            });
+            books.Add(new Book
+            {
+                id = 2,
+                title = "El Hobbit",
+                description = "Novela fantastica de J. R. R. Tolkien. ",
+                genre = "Fantasía",
+                publisher = "Editorial Atlantida"
+            });
+
+            books.Add(new Book
+            {
+                id = 3,
+                title = "El Código de Da Vinci",
+                description = "Novela de misterio del escritor Dan Brown. ",
+                genre = "Misterio",
+                publisher = "Editorial Estrada"
+            });
+        }
+
+        //
+        // GET: /Books/
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+
+        //
+        // GET: /Books/Details/5
+        public IHttpActionResult Details(int id)
+        {
+            Book book = books.FirstOrDefault((p) => p.id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(book);
+        }
+    }
+}
---
+++ Edicion La Plata/backend/HxLabs-LP/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HxLabs_LP.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string genre { get; set; }
+        public string publisher { get; set; }
+    }
+}
