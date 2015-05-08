3baf70c86da83fef236d27e1b65dba03526005e3
Eduardo Malvino <emalvino@hexacta.com>
2015-05-08 18:07:24 -0300

    model updated

--- dev/Tpl/src/main/java/ar/com/hexacta/tpl/model/User.java
+++ dev/Tpl/src/main/java/ar/com/hexacta/tpl/model/User.java
@@ -61,4 +61,20 @@ public class User implements Serializable {
         username = name;
     }
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
 }
